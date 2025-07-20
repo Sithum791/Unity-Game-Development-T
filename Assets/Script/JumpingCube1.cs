2ccc643bdc9ff431319adb78efbaa8928feb05ba
Sithum20010501 <cb8b11330@kiu.ac.lk>
2025-07-20 16:42:37 +0530

    Task 02

---
+++ Assets/Script/JumpingCube1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingCube1 : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
